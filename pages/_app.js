eaf482266c5ae8fad6d5e79bba85adeb3e5d8013
Andrew Bastin <andrewbastin.k@gmail.com>
2021-11-21 02:09:25 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
+import { initializeApp } from 'firebase/app'
+import { AppBar } from "@mui/material/AppBar"
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
-export default MyApp
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0EmXbXmaMxXwA9JvenDQrOb2rcERik8Q",
+  authDomain: "coursespace-lakehead.firebaseapp.com",
+  projectId: "coursespace-lakehead",
+  storageBucket: "coursespace-lakehead.appspot.com",
+  messagingSenderId: "106168839052",
+  appId: "1:106168839052:web:2bf8c0b1e2a170edd184a9"
+};
+
+initializeApp(firebaseConfig);
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Component {...pageProps} />
+  )
+}
---
+++ pages/admin.js
@@ -0,0 +1,61 @@
+// @ts-check
+import React from "react"
+
+import { getAuth, signOut } from "firebase/auth"
+import { useRouter } from "next/router"
+import { useAuthState } from "react-firebase-hooks/auth"
+import { AppBar, IconButton, Box, Button, Toolbar, Typography, List, ListSubheader, ListItem, ListItemText, ListItemButton, ListItemIcon } from "@mui/material"
+import { Menu, PersonAdd } from "@mui/icons-material"
+
+export default function Admin() {
+  const auth = getAuth()
+
+  const [user] = useAuthState(auth)
+  const router = useRouter()
+  
+  const onLogoutClick = () => {
+    signOut(auth)
+    router.replace("/")
+  }
+
+  return (
+    <Box>
+      <AppBar position={"sticky"}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Admin - {user?.email}
+          </Typography>
+          <Button color="inherit" onClick={onLogoutClick}>Logout</Button>
+        </Toolbar>
+      </AppBar>
+
+      <Box>
+        <List
+          subheader={
+            <ListSubheader style={{fontSize: 20 }}>
+              Actions
+            </ListSubheader>
+          }
+        >
+          <ListSubheader>
+            Accounts
+          </ListSubheader>
+
+          <ListItemButton>
+            <ListItemIcon>
+              <PersonAdd />
+            </ListItemIcon>
+            <ListItemText>Add Student Account</ListItemText>
+          </ListItemButton>
+
+          <ListItemButton>
+            <ListItemIcon>
+              <PersonAdd />
+            </ListItemIcon>
+            <ListItemText>Add Instructor Account</ListItemText>
+          </ListItemButton>
+        </List>
+      </Box>
+    </Box>
+  )
+}
